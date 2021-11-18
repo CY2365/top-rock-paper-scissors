36832d95befec90862194ec3a89bc3be3663cab6
CY2365 <ckyz1007@gmail.com>
2021-11-18 23:56:26 +0300

    Finish rock paper scissors project

--- app.js
+++ app.js
@@ -1,16 +1,83 @@
 const outcomes = ["Rock", "Paper", "Scissors"]
 
+// Randomly picks one of the three options from the outcomes array.
 function computerPlay() {
     return outcomes[Math.floor(Math.random() * outcomes.length)];
 }
 
-let playerPrompt = prompt("Choose 'Rock' 'Paper' or 'Scissors'.")
+function playRound() {
+    const userSelection = prompt('Choose Rock, Paper or Scissors..')
+                        .toLowerCase();
+    const computerSelection = computerPlay().toLowerCase();
+    
+    console.log(`You chose: ${userSelection}`);
+    console.log(`'AI' chose: ${computerSelection}.`);
 
-fuction playGame(userSelection, computerSelection) {
-    if (userSelection === computerSelection) {
-        return "It's a tie!"
-    } else if 
+    switch (computerSelection) {
+        // Computer has:
+        case 'rock':
+            // User has:
+            switch (userSelection) {
+                case 'rock':
+                    return 'It\'s a tie';
+                    break;
+                case 'paper':
+                    return 'You won';
+                    break;
+                case 'scissors':
+                    return 'You lost';
+                    break;
+                default:
+                    return 'Choose one';
+                    break;
+            }
+            break;
+        
+        // Computer has:
+        case 'paper':
+            // User has:
+            switch (userSelection) {
+                case 'rock':
+                    return 'You lost';
+                    break;
+                case 'paper':
+                    return 'It\'s a tie';
+                    break;
+                case 'scissors':
+                    return 'You won';
+                    break;
+                    default:
+                return 'Choose one';
+                break;
+            }
+            break;
+        
+        // Computer has:
+        case 'scissors':
+            // User has:
+            switch (userSelection) {
+                case 'rock':
+                    return 'You won';
+                    break;
+                case 'paper':
+                    return 'You lost';
+                    break;
+                case 'scissors':
+                    return 'It\'s a tie';
+                    break;
+                    default:
+                return 'Choose one';
+                break;
+            }
+            break; 
+    }
 }
 
+// Loops 5 times to playRound 5 times.
+function game() {
+    for (let times = 0; times < 5; times++) {
+    console.log(playRound())
+    }
+}
 
-console.log(computerPlay())
+game()
