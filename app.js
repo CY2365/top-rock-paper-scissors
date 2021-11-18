f4bb5649ac28c483e77c070d7c8bb72750ad2deb
Cenk <ckyz1007@gmail.com>
2021-11-18 10:50:08 +0300

    Start excercise

---
+++ app.js
@@ -0,0 +1,16 @@
+const outcomes = ["Rock", "Paper", "Scissors"]
+
+function computerPlay() {
+    return outcomes[Math.floor(Math.random() * outcomes.length)];
+}
+
+let playerPrompt = prompt("Choose 'Rock' 'Paper' or 'Scissors'.")
+
+fuction playGame(userSelection, computerSelection) {
+    if (userSelection === computerSelection) {
+        return "It's a tie!"
+    } else if 
+}
+
+
+console.log(computerPlay())
